4955fe40eee9c70b97c23dfaadc31a9db2c1f94f
Gabriel Mislang <gabrielmislang101@gmail.com>
2023-11-08 18:45:27 +0800

    Create createPhoneNumber.py

---
+++ Codewars/createPhoneNumber.py
@@ -0,0 +1,4 @@
+def create_phone_number(n):
+    return f"({''.join(map(str, n[0:3]))}) {''.join(map(str, n[3:6]))}-{''.join(map(str, n[6:10]))}"
+
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) # => returns "(123) 456-7890"
