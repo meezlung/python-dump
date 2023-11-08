4999c0dbc0f77c3170793690f4819138cd98aee7
Gabriel Mislang <gabrielmislang101@gmail.com>
2023-11-08 19:54:11 +0800

    Update sortTheOdd.py

--- Codewars/sortTheOdd.py
+++ Codewars/sortTheOdd.py
@@ -1,15 +1,30 @@
-def sort_array(source_array):
-    odd = {}
-
+def sort_array_draft(source_array):
+    odd = []
+    odd_index = []
+    index_tracker = 0
     for number in source_array:
         if number % 2 != 0:
-            odd[number] = source_array.index(number)
+            odd.append(number)
+            odd_index.append(index_tracker)
+            index_tracker += 1
+        else:
+            index_tracker += 1
+    odd.sort()
 
-    sorted_odd = dict(sorted(odd.items()))    
+    for index in odd_index:
+        source_array[index] = odd[odd_index.index(index)]
 
-    return source_array, sorted_odd
+    return source_array
+
+def sort_array(source_array):
+    result = sorted([l for l in source_array if l % 2 == 1])
+    print(result)
+    for index, item in enumerate(source_array):
+        print(index, item)
+        if item % 2 == 0:
+            result.insert(index, item)
+    return result
             
-
-print(sort_array([7, 1])) # [7, 1]  =>  [1, 7]
+print(sort_array([1, -22, -24, 5, 19, -3, 46, -22, 1])) 
 print(sort_array([5, 8, 6, 3, 4])) # [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 print(sort_array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])) # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
