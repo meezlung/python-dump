dac6c37794f703fe080c93e656d69a3c58ce45b1
Gabriel Mislang <gabrielmislang101@gmail.com>
2023-11-08 18:56:43 +0800

    Create uniqueInOrder.py

---
+++ Codewars/uniqueInOrder.py
@@ -0,0 +1,17 @@
+def unique_in_order(sequence):
+    tracker = ""
+    final_answer = []
+    for seq in sequence:
+        if seq != tracker:
+            final_answer.append(seq)
+            tracker = seq
+    return(final_answer)
+
+        
+
+        
+
+
+print(unique_in_order('AAAABBBCCDAABBB'))
+print(unique_in_order('ABBCcAD'))
+print(unique_in_order([1, 2, 2, 3, 3]))
