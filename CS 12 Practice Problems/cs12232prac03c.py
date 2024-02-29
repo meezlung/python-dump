4f93ba482593aff3fed33eb64943b8a6e759fca9
Gabriel Mislang <65329581+meezlung@users.noreply.github.com>
2024-02-29 10:21:48 +0000

    Change Prac03C

--- CS 12 Practice Problems/cs12232prac03c.py
+++ CS 12 Practice Problems/cs12232prac03c.py
@@ -12,30 +12,26 @@ class Movements:
         self.y: int = point.y
         self.z: int = point.z
 
-        self.visited_points: set[Point] = set()
+        self.visited_points: set[Point] = {point}
         self.visited_points.add(point)
 
     def move_to(self, dir: str) -> None: 
-        x, y, z = self.x, self.y, self.z
         if dir == "+x":
-            x += 1
+            self.x += 1
         if dir == "-x":
-            x -= 1
+            self.x -= 1
         if dir == "+y":
-            y += 1
+            self.y += 1
         if dir == "-y":
-            y -= 1
+            self.y -= 1
         if dir == "+z":
-            z += 1
+            self.z += 1
         if dir == "-z":
-            z -= 1
-        else:
-            return None
-        
-        new_point = Point(x, y, z)
-        self.x, self.y, self.z = new_point
+            self.z -= 1
+
+        new_point = Point(self.x, self.y, self.z)
         self.visited_points.add(new_point)
-        
+
     def teleport_to(self, point: Point) -> None:
         self.x, self.y, self.z = point.x, point.y, point.z
         self.visited_points.add(point)
