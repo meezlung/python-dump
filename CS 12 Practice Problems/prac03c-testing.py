d17d459f9c142415c99541d0df273f00ad11b82b
Gabriel Mislang <65329581+meezlung@users.noreply.github.com>
2024-02-29 10:22:40 +0000

    Change Prac03C Smoke Test

--- CS 12 Practice Problems/prac03c-testing.py
+++ CS 12 Practice Problems/prac03c-testing.py
@@ -1,10 +1,22 @@
-from cs12232prac03c import Movements, Point
-   
-test0 = Movements(Point(1, 2, 3))
-test1 = Movements(Point(10, -30, 0))
-test2 = Movements(Point(69, 420, 41))
+from cs12232prac03c import Point, Movements
 
-test0.move_to("+x")
-test0.move_to("-x")
+m = Movements(Point(10, -30, 0))
 
-print(test0.visited_points)
+m.move_to('+x')
+m.move_to('+z')
+m.move_to('+z')
+m.move_to('-z')
+m.move_to('-y')
+m.teleport_to(Point(100, 100, 100))
+
+
+assert m.visited_count() == 6
+
+assert m.visited() == {
+    Point(100, 100, 100),
+    Point(10, -30, 0),
+    Point(11, -30, 0),
+    Point(11, -30, 1),
+    Point(11, -30, 2),
+    Point(11, -31, 1),
+}
