c0dd048b582f665af17088a71bbf06bc76a7a22a
Gabriel Mislang <65329581+meezlung@users.noreply.github.com>
2024-02-29 10:21:43 +0000

    Change Prac03B

--- CS 12 Practice Problems/cs12232prac03b.py
+++ CS 12 Practice Problems/cs12232prac03b.py
@@ -1,23 +1,19 @@
-from cs12232prac03a import MinReadyArray
-
 class Metropolis:
     def __init__(self, H: list[int]) -> None:
-        self.m = H
-        self.total_cost = 0
+        self.m: list[int] = H
     
     def expand_left(self, h: int) -> None:
-        MinReadyArray.append_left(h)
+        self.m.insert(0, h)
 
     def __len__(self) -> int:
         return len(self.m)
     
     def cost(self, i: int, j: int) -> int | None:
-        if 0 <= i <= j <= len(self.m):
-            min_value = MinReadyArray.min(self, i, j)
-
-            for x in range(len(self.m)):
-                difference = abs(self.m[x] - min_value)
-                self.total_cost += difference
-            
-            return self.total_cost * 7
+        if 0 <= i <= j < len(self.m) and self.m:
+            total_cost = 0
+            for x in range(i, j+1):
+                if self.m[x] != min(self.m[i:j+1]):
+                    difference = abs(self.m[x] - min(self.m[i:j+1]))
+                    total_cost += difference
+            return total_cost * 7
         return None
