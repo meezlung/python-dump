47471125a1956a2074dd9af3c2c7e887cf959ec5
Gabriel Mislang <65329581+meezlung@users.noreply.github.com>
2024-02-29 10:22:10 +0000

    Change Prac03B Smoke Test

--- CS 12 Practice Problems/prac03b-testing.py
+++ CS 12 Practice Problems/prac03b-testing.py
@@ -1,5 +1,11 @@
 from cs12232prac03b import Metropolis
 
-test1 = Metropolis([4, 3, 5, 2, 1])
+m = Metropolis([3, 1, 4, 1, 5])
 
-print(test1.cost(0, 5))
+assert m.cost(0, 0) == 0
+assert m.cost(0, 1) == 14
+assert m.cost(0, 2) == 35
+assert len(m) == 5
+m.expand_left(2)
+assert len(m) == 6
+assert m.cost(1, 3) == 35
