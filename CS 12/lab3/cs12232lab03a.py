96e5b9e81de431d3e8d256cd664fa3d2a47b9a13
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-03-13 12:09:54 +0800

    Update cs12232lab03a.py

--- CS 12/lab3/cs12232lab03a.py
+++ CS 12/lab3/cs12232lab03a.py
@@ -3,43 +3,37 @@ from cs12232prac03a import MinReadyArray
 class MaxReadyArray:
     def __init__(self):
         self.m: MinReadyArray = MinReadyArray()
+        self.m_copy: MinReadyArray = MinReadyArray()
 
     def append_right(self, value: int) -> None:
-        self.m.append_right(value)
+        self.m.append_right(value * -1)
 
     def append_left(self, value: int) -> None:
-        self.m.append_left(value)
+        self.m.append_left(value * -1)
 
     def pop_left(self) -> int | None:
-        return self.m.pop_left()
+        number: int | None = self.m.pop_left() 
+        if number is not None:
+            return number * -1
+        return None
 
     def pop_right(self) -> int | None:
-        return self.m.pop_right()
+        number: int | None = self.m.pop_right() 
+        if number is not None:
+            return number * -1
+        return None
 
     def __len__(self) -> int:
         return len(self.m)
     
     def __getitem__(self, i: int) -> int | None:
-        return self.m[i]
+        number: int | None = self.m[i]
+        if number is not None:
+            return number * -1
+        return None
     
     def max(self, i: int, j: int) -> int | None:
-        if max(i, 0) < min(j, len(self.m)):
-            self.negate(max(i, 0), min(j, len(self.m)))
-
-            minimum: int | None = self.m.min(i, j)
-
-            self.negate(max(i, 0), min(j, len(self.m)))
-
-            return minimum * -1 if minimum is not None else None
-        
-        else:
-            return None
-
-    def negate(self, i: int, j: int):
-        x: int = 0
-        while x < len(self.m):
-            number: int | None = self.m[0]
-            if number is not None:
-                self.m.append_right(number * -1)
-                self.m.pop_left()
-                x += 1
+        minimum: int | None = self.m.min(i, j)
+        if minimum is not None:
+            return minimum * -1
+        return None
