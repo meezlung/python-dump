8add5e88402d694199410b77612335c4d7a90b98
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-03-13 23:32:26 +0800

    Update cs12232prac04a.py

--- CS 12/cs12232prac04a.py
+++ CS 12/cs12232prac04a.py
@@ -2,5 +2,11 @@ from typing import Sequence, TypeVar
 
 T = TypeVar('T')
 
-def stack_like_pancakes(l: Sequence[list[T]]) -> list[T] | None:
-    return [e for elem in l for e in reversed(elem)]
+def stack_like_pancakes(l: Sequence[list[T]]) -> list[T]:
+    stack: list[T] = []
+
+    for elem in l:
+        for e in reversed(elem):
+            stack.append(e)
+    
+    return stack 
