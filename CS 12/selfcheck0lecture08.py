dd8e24b663bbeb0c43a29e22b6dcd0591b3d4c30
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-03-13 16:55:48 +0800

    Create selfcheck0lecture08.py

---
+++ CS 12/selfcheck0lecture08.py
@@ -0,0 +1,11 @@
+def f(seq: list[int]) -> list[int]:
+    return seq[::-1]
+
+def f1(xss: list[list[int]]) -> list[int]:
+    return [x for xs in xss for x in xs]
+
+def f2(m: dict[int, int]) -> dict[int, int]:
+    ret: dict[int, int] = {}
+
+    return ret
+
