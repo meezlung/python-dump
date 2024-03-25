7c63489aabdece20aa58f9a64cb24e6a6be34156
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-03-25 18:53:05 +0800

    Organize everything

--- CS 12/selfcheck/selfcheck1a.py
+++
@@ -1,9 +0,0 @@
-class CapsLock:
-    def __init__(self, string: str) -> None:
-        self.string = string
-
-    def uppercase(self) -> str:
-        return self.string.upper()
-    
-c = CapsLock('abc')
-print(c.uppercase())
