f8d54fb2f6bbd818fe20463e815878d8a2003f57
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-12 13:37:43 +0800

    Create theObservedPin.py

---
+++ Codewars/theObservedPin.py
@@ -0,0 +1,37 @@
+# TODO
+# 1. Iterate through each digit
+# 2. For each digit, check all their adjacents in a hashmap
+# 3. Use itertools for each lists in the hashmaps
+# Time Complexity: O(n^k)
+# Space Complexity: O(n)
+
+from itertools import product
+
+def get_pins(observed):
+    adjacent_digits = {'1': ['1', '2', '4'], 
+                       '2': ['1', '2', '3', '5'], 
+                       '3': ['2', '3', '6'], 
+                       '4': ['1', '4', '5', '7'], 
+                       '5': ['2', '4', '5', '6', '8'], 
+                       '6': ['3', '5', '6', '9'], 
+                       '7': ['4', '7', '8'], 
+                       '8': ['5', '7', '8', '9', '0'], 
+                       '9': ['6', '8', '9'], 
+                       '0': ['0', '8']}
+    
+    permutable_digits = []
+
+    for digit in observed:
+        permutable_digits.append(adjacent_digits[digit])
+
+    answer = []
+
+    for each_combination in list(product(*permutable_digits)):
+        answer.append(''.join(each_combination))
+
+    return answer
+
+
+print(get_pins('8'))
+print(get_pins('11'))
+print(get_pins('369'))
