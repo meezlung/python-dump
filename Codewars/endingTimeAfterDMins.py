5552fb026a2ea732f96466de80afb8ace470cd56
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-12 12:45:52 +0800

    Update endingTimeAfterDMins.py

--- Codewars/endingTimeAfterDMins.py
+++ Codewars/endingTimeAfterDMins.py
@@ -1,17 +1,17 @@
 time = input().split(':')
 minutes = int(input())
+new_time = []
 
-for x in range(0, len(time)):
-    time[x] = int(time[x])
-print(time)
+for t in time:
+    new_time.append(int(t))
 
-time[1] += minutes
-if time[1] >= 60:
-    howManyMins = time[1] // 60
+new_time[1] += minutes
+if new_time[1] >= 60:
+    howManyMins = new_time[1] // 60
     time[1] -= 60*howManyMins
     time[0] += howManyMins
-if time[0] >= 24:
-    howManyHours = time[0] // 24
+if new_time[0] >= 24:
+    howManyHours = new_time[0] // 24
     time[0] = time[0] - 24*howManyHours
 
 newTime = [str(t).zfill(2) for t in time]
