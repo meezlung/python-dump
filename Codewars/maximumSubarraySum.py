0c76b5cf71ebfbf8f84bde8cb48b0f0002553378
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-13 15:35:07 +0800

    Update maximumSubarraySum.py

--- Codewars/maximumSubarraySum.py
+++ Codewars/maximumSubarraySum.py
@@ -17,7 +17,7 @@ def max_sequence_inefficient_bruteforce(arr):
     
     return max_sum
 
-def max_sequence(arr):
+def max_sequence_long(arr):
     n = len(arr)
     max_ending_here = 0
     max_so_far = float('-inf')
@@ -40,7 +40,18 @@ def max_sequence(arr):
 
     return max_so_far
 
+def max_sequence(arr):
+    n = len(arr)
+    max_ending_here = 0
+    max_so_far = float('-inf')
+    negative_counter = 0 
 
+    for i in range(n):
+        if arr[i] < 0: negative_counter += 1
+        max_ending_here = max(arr[i], max_ending_here + arr[i])
+        max_so_far = max(max_so_far, max_ending_here)
+
+    return max_so_far if n != negative_counter else 0
     
 print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6)
 print(max_sequence([-2, -1, -3, -4, -1, -2, -1, -5, -4]), 0)
