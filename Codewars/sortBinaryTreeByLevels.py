22b3ac5e56f76c203991edd2edfd13a32b389317
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-20 02:39:04 +0800

    Create sortBinaryTreeByLevels.py

---
+++ Codewars/sortBinaryTreeByLevels.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+
+def tree_by_levels(node):
+    if node == None:
+        return []
+
+    add = []
+
+    q = [node]
+
+    while q:
+        node = q.pop(0)
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+
+        add.append(node.value)
+
+    return add
+
+print(tree_by_levels(None), [])
+print(tree_by_levels(Node(Node(None, Node(None, None, 4), 2), Node(Node(None, None, 5), Node(None, None, 6), 3), 1)), [1, 2, 3, 4, 5, 6])
